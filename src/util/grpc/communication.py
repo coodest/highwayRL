1c03ff3a7eb09d2dccd3b2f6daeca5f358ac2f1b
heptagram <350526878@qq.com>
2021-05-27 22:24:02 +0800

    210525 add gRPC

--- src/util/grpc/communication.py
+++ src/util/grpc/communication.py
@@ -1,19 +1,18 @@
-import multiprocessing as mp
 import os
 from src.module.agent.policy import Policy
-import grpc
 
-import src.util.grpc as g
+from src.util.grpc.communication_pb2_grpc import *
+from src.util.grpc.communication_pb2 import *
 from concurrent import futures
 
 
 class Client:
     def __init__(self, server_address):
         channel = grpc.insecure_channel(server_address)
-        self.stub = g.communication_pb2_grpc.CommunicationStub(channel)
+        self.stub = CommunicationStub(channel)
 
     def comm(self, last_obs, action, obs, reward, add):
-        reply = self.stub.comm(g.communication_pb2.CommRequest(
+        reply = self.stub.comm(CommRequest(
             last_obs=last_obs,
             action=action,
             obs=obs,
@@ -30,7 +29,7 @@ class Server:
 
     def start(self):
         server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
-        g.communication_pb2_grpc.add_CommunicationServicer_to_server(Policy(), server)
+        add_CommunicationServicer_to_server(Policy(), server)
         server.add_insecure_port(self.server_address)
         server.start()
         server.wait_for_termination()
