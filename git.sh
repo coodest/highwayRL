a8d39f9dc6e1781bcea7f898f754c8f09872f9a5
Heptagram <350526878@qq.com>
2021-06-19 17:25:56 +0930

    210610 determ mcts (over-estimated) done

--- git.sh
+++ git.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
-## 1. commit with context information
+# 1. commit with context information
 git config --global credential.helper store
 git config --global user.email 350526878@qq.com
-git config --global user.name heptagram
+git config --global user.name Heptagram
 git rm -r --cached .
 git add .
+git update-index --chmod=+x run.sh
+git update-index --chmod=+x git.sh
 git --no-pager diff --cached ./
 commit_label=$(head -n 1 ./CHANGELOG)
 git commit -m "${commit_label}"
 git push -u origin master
 
-## 2. show time
+# 2. show time
 date
 
