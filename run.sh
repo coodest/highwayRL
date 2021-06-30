71e41c07b8d1bd0906e01d83c4abbd65cb8af214
Heptagram <350526878@qq.com>
2021-06-30 07:25:24 +0000

    210629 add mixed sync mode

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-python -X pycache_prefix=./cache -m src.app.memrl atari
+python -X pycache_prefix=./cache -m src.app.memrl 1
+python -X pycache_prefix=./cache -m src.app.memrl 2
+python -X pycache_prefix=./cache -m src.app.memrl 3
