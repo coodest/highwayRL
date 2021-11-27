41a618f88f5cae7dfb9e71376158a174a3726094
Heptagram <350526878@qq.com>
2021-11-27 13:00:08 +0000

    211127 rethinking state modelling

--- git.sh
+++ git.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
-# 1. git puu
-echo "pull"
-git pull --rebase
-
-# 2. commit with context information
+# commit with context information
 echo "push"
 git config --global credential.helper store
 git config --global user.email 350526878@qq.com
@@ -19,6 +15,6 @@ commit_label=$(head -n 1 ./CHANGELOG)
 git commit -m "${commit_label}"
 git push -u origin master
 
-# 3. show time
+# show time
 date
 
