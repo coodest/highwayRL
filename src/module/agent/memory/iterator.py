b032c8fb991e4740c388d0056c32beb061a82932
Heptagram <350526878@qq.com>
2021-12-05 10:36:15 +0000

    211205 add cuda mps

--- src/module/agent/memory/iterator.py
+++ src/module/agent/memory/iterator.py
@@ -14,23 +14,23 @@ class Iterator:
         self.device = torch.device(f"cuda:{ind}" if torch.cuda.is_available() else "cpu")
 
     def iterate(self, np_adj, np_rew, np_val_0):
-        adj = torch.from_numpy(np_adj).to(self.device)
-        rew = torch.from_numpy(np_rew).to(self.device)
-        val = torch.from_numpy(np_val_0).to(self.device)
-
-        iters = 0
-        while iters < P.max_vp_iter:
-            iters += 1
-            last_val = val
-            val = torch.max(adj * val, dim=1).values * P.gamma + rew
-            if torch.sum(last_val - val) == 0:
-                break
-        Logger.log(f"iters: {iters}", color="yellow")
-        result = val.cpu().detach().numpy().tolist()
-
-        # release resorces
-        del adj, rew, val, last_val
         with torch.no_grad():
+            adj = torch.from_numpy(np_adj).to(self.device)
+            rew = torch.from_numpy(np_rew).to(self.device)
+            val = torch.from_numpy(np_val_0).to(self.device)
+
+            iters = 0
+            while iters < P.max_vp_iter:
+                iters += 1
+                last_val = val
+                val = torch.max(adj * val, dim=1).values * P.gamma + rew
+                if torch.sum(last_val - val) == 0:
+                    break
+            Logger.log(f"iters: {iters}", color="yellow")
+            result = val.cpu().detach().numpy().tolist()
+
+            # release resorces
+            del adj, rew, val, last_val
             torch.cuda.empty_cache()
 
         return result
--- src/module/agent/memory/projector.py
+++ src/module/agent/memory/projector.py
@@ -7,11 +7,13 @@ from src.util.tools import Logger, Funcs
 class RandomMatrix(torch.nn.Module):
     def __init__(self, inf, outf):
         super().__init__()
-        self.layer = torch.nn.Linear(in_features=inf, out_features=outf)
-        torch.nn.init.xavier_uniform_(self.layer.weight)
+        with torch.no_grad():
+            self.layer = torch.nn.Linear(in_features=inf, out_features=outf)
+            torch.nn.init.xavier_uniform_(self.layer.weight)
 
     def forward(self, input):
-        return self.layer(input)
+        with torch.no_grad():
+            return self.layer(input)
 
 
 class Projector:
@@ -43,25 +45,26 @@ class RandomProjector(Projector):
     def batch_project(self, obs_list):     
         batch = np.vstack(obs_list)  
         input = None
-        if P.env_type == "atari":
-            # [2, P.screen_size * P.screen_size]
-            input = torch.tensor(batch, dtype=torch.float).to(self.device)
-
-        with torch.no_grad():  # no grad calculation
+        with torch.no_grad():
+            if P.env_type == "atari":
+                # [2, P.screen_size * P.screen_size]
+                input = torch.tensor(batch, dtype=torch.float, requires_grad=False).to(self.device)
             output = self.random_matrix(input)
+            result = output.cpu().detach().numpy().tolist()
 
-        return output.cpu().detach().numpy().tolist()
+        return result
 
 
 class RNNProjector(Projector):
     def __init__(self, id) -> None:
         super().__init__(id)
         self.random_matrix = None
-        self.hidden_0 = torch.rand(P.projected_hidden_dim).to(self.device)
-        self.hidden = None
+        with torch.no_grad():
+            self.hidden_0 = torch.rand(P.projected_hidden_dim, requires_grad=False).to(self.device)
+            self.hidden = None
+            self.reset()
         if P.env_type == "atari":
             self.random_matrix = RandomMatrix(P.screen_size * P.screen_size + P.projected_hidden_dim, P.projected_dim + P.projected_hidden_dim).to(self.device)
-        self.reset()
         self.last_result = np.zeros(P.projected_dim)
 
     def reset(self):
@@ -71,21 +74,21 @@ class RNNProjector(Projector):
     def project(self, obs):
         batch = obs
         input = None
-        if P.env_type == "atari":
-            # [P.screen_size * P.screen_size]
-            input = torch.tensor(batch, dtype=torch.float).to(self.device)
-            # [P.screen_size * P.screen_size + hidden_dim]
-            input = torch.cat([input, self.hidden], dim=0)
-            # [1, P.screen_size * P.screen_size + hidden_dim]
-            input = input.unsqueeze(0)
-
         with torch.no_grad():  # no grad calculation
+            if P.env_type == "atari":
+                # [P.screen_size * P.screen_size]
+                input = torch.tensor(batch, dtype=torch.float, requires_grad=False).to(self.device)
+                # [P.screen_size * P.screen_size + hidden_dim]
+                input = torch.cat([input, self.hidden], dim=0)
+                # [1, P.screen_size * P.screen_size + hidden_dim]
+                input = input.unsqueeze(0)
+
             # [1, projected_dim + hidden_dim]
             output = self.random_matrix(input)
             self.hidden = output[0, P.projected_dim:]
             output = output[0, :P.projected_dim]
 
-        result = output.cpu().detach().numpy().tolist()
+            result = output.cpu().detach().numpy().tolist()
         self.last_result = result.copy()
 
         return result
@@ -101,18 +104,19 @@ class CNNProjector(Projector):
     def __init__(self, id) -> None:
         super().__init__(id)
 
-        self.conv1 = torch.nn.Conv2d(1, 1, kernel_size=(6, 6), stride=(5, 5), dilation=(2, 2)).to(self.device)
-        self.conv2 = torch.nn.Conv2d(1, 1, kernel_size=(3, 3), stride=(5, 5), dilation=(2, 2)).to(self.device)
+        with torch.no_grad():
+            self.conv1 = torch.nn.Conv2d(1, 1, kernel_size=(6, 6), stride=(5, 5), dilation=(2, 2)).to(self.device)
+            self.conv2 = torch.nn.Conv2d(1, 1, kernel_size=(3, 3), stride=(5, 5), dilation=(2, 2)).to(self.device)
 
     def batch_project(self, obs_list):
-        # [2, P.screen_size * P.screen_size]
-        batch = np.vstack(obs_list)  
-        input = torch.tensor(batch, dtype=torch.float, requires_grad=False).to(self.device)
-        input = input.unsqueeze(1)
-        # [2, 1, P.screen_size, P.screen_size]
-        input = input.reshape(input.shape[0], input.shape[1], P.screen_size, -1)
-
         with torch.no_grad():  # no grad calculation
+            # [2, P.screen_size * P.screen_size]
+            batch = np.vstack(obs_list)  
+            input = torch.tensor(batch, dtype=torch.float, requires_grad=False).to(self.device)
+            input = input.unsqueeze(1)
+            # [2, 1, P.screen_size, P.screen_size]
+            input = input.reshape(input.shape[0], input.shape[1], P.screen_size, -1)
+
             # [2, 1, 15, 15]
             output = self.conv1(input)
             # [2, 1, 3, 3]
@@ -121,5 +125,6 @@ class CNNProjector(Projector):
             output = output.squeeze(1)
             # [2, 9]
             output = torch.flatten(output, start_dim=1)
+            result = output.cpu().detach().numpy().tolist()  # detach to remove grad_fn
 
-        return output.cpu().detach().numpy().tolist()  # detach to remove grad_fn
+        return result
