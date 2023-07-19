68bc24561756459a17ab5a9e57370850ef6794f3
Heptagram <350526878@qq.com>
2023-07-19 15:09:19 +0800

    230719 remove requirements.txt

--- run.sh
+++ run.sh
@@ -9,36 +9,11 @@ xhost +"local:docker@"
 # run tensorboard (run in a separate terminal)
 # tensorboard --logdir ./output/
 
-# profile ID
-# PROFILE=1
-# PROFILE=2
-# PROFILE=3
-# PROFILE=4
-# PROFILE=5
-# PROFILE=6
-# PROFILE=7
-# PROFILE=8
-# PROFILE=9
-# PROFILE=10
-# PROFILE=11
-# PROFILE=12
-# PROFILE=13
-# PROFILE=14
-# PROFILE=15
-# PROFILE=16
-# PROFILE=17
-# PROFILE=18
-# PROFILE=19
-# PROFILE=20
-# PROFILE=21
-# PROFILE=22
-# PROFILE=23
-PROFILE=24
-# PROFILE=25
-# PROFILE=26
-
 # delete output folder
 docker run --user=user --volume $(pwd):/work --rm --interactive --tty rl rm -rf ./output
 
-# run code
+# run code: like for PROFILE in {1..10} 24 25
+for PROFILE in 24
+do
 docker run --user=user --runtime=nvidia --gpus all --env DISPLAY=$DISPLAY --env="MPLCONFIGDIR=/tmp/matplotlib" --env="NVIDIA_DRIVER_CAPABILITIES=all" --volume /tmp/.X11-unix:/tmp/.X11-unix:rw --volume $(pwd):/work --rm --interactive --tty rl python -X pycache_prefix=./cache -m src.app.memrl --profile $PROFILE
+done
