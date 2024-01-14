68e57effed629b234a1c545ba6acc3fa99fed40d
Heptagram <350526878@qq.com>
2024-01-14 15:12:33 +0000

    231229 re-implement the graph

--- git.sh
+++ git.sh
@@ -4,11 +4,9 @@ echo "push"
 git config --global credential.helper store
 git config --global user.email 350526878@qq.com
 git config --global user.name Heptagram
+git config core.filemode false
 git rm -r --cached .
 git add .
-git update-index --chmod=+x run.sh
-git update-index --chmod=+x git.sh
-git update-index --chmod=+x test.sh
 git --no-pager diff --cached ./
 commit_label=$(head -n 1 ./CHANGELOG)
 git commit -m "${commit_label}"
