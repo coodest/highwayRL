3b719b06a455b03d3700ce778f5d507e29a7fdb9
Heptagram <350526878@qq.com>
2024-02-04 09:05:51 +0000

    240127 re-implement the dnn parameterizer

--- run.sh
+++ run.sh
@@ -16,20 +16,19 @@ PARALLEL=--tty
 # PARALLEL=--detach
 
 # for ENV_TYPE in atari maze toy_text football
-for ENV_TYPE in football
+for ENV_TYPE in maze toy_text
 do
-    # for RUN in {0..9}
-    for RUN in 0
+    for RUN in {0..9}
     do
         for ENV in $(sed 1d ./assets/${ENV_TYPE}.txt)
         do
             docker run --user=worker --gpus all --env DISPLAY=$DISPLAY --env="MPLCONFIGDIR=/tmp/matplotlib" --env="NVIDIA_DRIVER_CAPABILITIES=all" --shm-size=40gb --volume /tmp/.X11-unix:/tmp/.X11-unix:rw --volume $(pwd):/home/worker/work --rm --interactive $PARALLEL meetingdocker/rl:pt_0.1 python -X pycache_prefix=./cache -m src.app.highwayrl --env_type $ENV_TYPE --env_name $ENV --run $RUN
             break
         done
-    done
-    while [ `docker ps --all | wc -l` -gt 1 ]
-    do 
-        sleep 1
+        while [ `docker ps --all | wc -l` -gt 1 ]
+        do 
+            sleep 1
+        done
     done
     echo "${ENV_TYPE} done"
 done
