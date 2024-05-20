637b049598400e7a9e503594a4a0f268b3c7252f
coodest <350526878@qq.com>
2024-05-20 11:13:38 +0800

    Update iterator.py

--- src/module/agent/policy/iterator.py
+++ src/module/agent/policy/iterator.py
@@ -20,9 +20,6 @@ class Iterator:
     
     def iterate(self, np_rew, np_gamma, np_val_0, np_adj):
         with torch.no_grad():
-            # debug: limit the VRam size
-            # torch.cuda.set_per_process_memory_fraction(0.5)
-
             rew = torch.from_numpy(np_rew).to(self.device)
             gamma = torch.from_numpy(np_gamma).to(self.device)
             val = torch.from_numpy(np_val_0).to(self.device)
@@ -32,25 +29,8 @@ class Iterator:
             while n_iters < P.max_vp_iter:
                 n_iters += 1
                 last_val = val
-
-                # mul = None
-                # pro = None
                 divider = 1
-                # while True:
-                #     try:
-                #         last_position = 0
-                #         divided_len = int(len(adj) / divider)
-                #         mul = torch.tensor([], dtype=val.dtype, device=val.device)
-                #         while True:
-                #             pro = torch.max(adj[last_position:last_position + divided_len] * val, dim=1).values
-                #                  break
-                #         break
-                #     except RuntimeError:
-                #         mul = None
-                #         pro = None
-                #         divider *= 2
-
-                # val = mul * gamma + rew
+                
                 max_inherit_value = torch.max((adj - 1) * 1e31 + torch.mul(adj, val) * gamma.view([-1, 1]), dim=1).values
                 val = torch.where(max_inherit_value == -1e31, 0.0, max_inherit_value) + rew
 
